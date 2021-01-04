c6b57030f597d66e445fab730fc7cf66832aa8ac
Anshuman46 <75122178+Anshuman46@users.noreply.github.com>
2021-01-04 16:56:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,64 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+
+var ball,ground;
+var log1,log2,log3;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	ball = new Paper(200,200,50/2);
+
+	log1 = new Dustbin(600,520,10,100);
+	log2 = new Dustbin(650,580,100,10);
+	log3 = new Dustbin(700,520,10,100);
+
+
+	ground = new Ground(400,600,800,20);
+
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  ball.display();
+  ground.display();
+  log1.display();
+  log2.display();
+  log3.display();
+
+  
+
+
+  drawSprites();
+ 
+}
+
+function keyPressed(){
+	if(keyCode === UP_ARROW){
+		Matter.Body.applyForce(ball.body, ball.body.position,{x:15,y:-25});
+	}
+}
+
+
+
